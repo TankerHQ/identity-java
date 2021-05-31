28a9f73bae4bce5d81f45bc83708b01c0e63e389
Timothée Isnard <timothee.isnard@tanker.io>
2021-05-31 16:03:39 +0200

    test: Serialization test vectors roundtrip, and upgrade function

--- src/test/java/io/tanker/identity/IdentityTest.java
+++ src/test/java/io/tanker/identity/IdentityTest.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.Base64;
 import java.util.function.Function;
 
+import static io.tanker.identity.Identity.upgradeIdentity;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -20,7 +21,25 @@ public class IdentityTest {
     private static final String appId = "tpoxyNzh0hU9G2i9agMvHyyd+pO6zGCjO9BfhrCLjd4=";
     private static final String appSecret = "cTMoGGUKhwN47ypq4xAXAtVkNWeyUtMltQnYwJhxWYSvqjPVGmXd2wwa7y17QtPTZhn8bxb015CZC/e4ZI7+MQ==";
 
+    private static final String PERMANENT_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJ1c2VyIiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSIsImRlbGVnYXRpb25fc2lnbmF0dXJlIjoiVTlXUW9sQ3ZSeWpUOG9SMlBRbWQxV1hOQ2kwcW1MMTJoTnJ0R2FiWVJFV2lyeTUya1d4MUFnWXprTHhINmdwbzNNaUE5cisremhubW9ZZEVKMCtKQ3c9PSIsImVwaGVtZXJhbF9wdWJsaWNfc2lnbmF0dXJlX2tleSI6IlhoM2kweERUcHIzSFh0QjJRNTE3UUt2M2F6TnpYTExYTWRKRFRTSDRiZDQ9IiwiZXBoZW1lcmFsX3ByaXZhdGVfc2lnbmF0dXJlX2tleSI6ImpFRFQ0d1FDYzFERndvZFhOUEhGQ2xuZFRQbkZ1Rm1YaEJ0K2lzS1U0WnBlSGVMVEVOT212Y2RlMEhaRG5YdEFxL2RyTTNOY3N0Y3gwa05OSWZodDNnPT0iLCJ1c2VyX3NlY3JldCI6IjdGU2YvbjBlNzZRVDNzMERrdmV0UlZWSmhYWkdFak94ajVFV0FGZXh2akk9In0=";
+    private static final String PROVISIONAL_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJlbWFpbCIsInZhbHVlIjoiYnJlbmRhbi5laWNoQHRhbmtlci5pbyIsInB1YmxpY19lbmNyeXB0aW9uX2tleSI6Ii8yajRkSTNyOFBsdkNOM3VXNEhoQTV3QnRNS09jQUNkMzhLNk4wcSttRlU9IiwicHJpdmF0ZV9lbmNyeXB0aW9uX2tleSI6IjRRQjVUV212Y0JyZ2V5RERMaFVMSU5VNnRicUFPRVE4djlwakRrUGN5YkE9IiwicHVibGljX3NpZ25hdHVyZV9rZXkiOiJXN1FFUUJ1OUZYY1hJcE9ncTYydFB3Qml5RkFicFQxckFydUQwaC9OclRBPSIsInByaXZhdGVfc2lnbmF0dXJlX2tleSI6IlVtbll1dmRUYUxZRzBhK0phRHBZNm9qdzQvMkxsOHpzbXJhbVZDNGZ1cVJidEFSQUc3MFZkeGNpazZDcnJhMC9BR0xJVUJ1bFBXc0N1NFBTSDgydE1BPT0ifQ==";
+    private static final String PUBLIC_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJ1c2VyIiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSJ9";
+    private static final String PUBLIC_PROVISIONAL_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJlbWFpbCIsInZhbHVlIjoiYnJlbmRhbi5laWNoQHRhbmtlci5pbyIsInB1YmxpY19lbmNyeXB0aW9uX2tleSI6Ii8yajRkSTNyOFBsdkNOM3VXNEhoQTV3QnRNS09jQUNkMzhLNk4wcSttRlU9IiwicHVibGljX3NpZ25hdHVyZV9rZXkiOiJXN1FFUUJ1OUZYY1hJcE9ncTYydFB3Qml5RkFicFQxckFydUQwaC9OclRBPSJ9";
+
     private static final LazySodiumJava LazySodium = new LazySodiumJava(new SodiumJava());
+
+    @Test
+    public void testIdentitySerializationRoundtrip() throws IOException {
+        String upgradedPermanent = upgradeIdentity(PERMANENT_IDENTITY);
+        String upgradedProvisional = upgradeIdentity(PROVISIONAL_IDENTITY);
+        String upgradedPublic = upgradeIdentity(PUBLIC_IDENTITY);
+        String upgradedPublicProvisional = upgradeIdentity(PUBLIC_PROVISIONAL_IDENTITY);
+
+        assertEquals(upgradedPermanent, PERMANENT_IDENTITY);
+        assertEquals(upgradedProvisional, PROVISIONAL_IDENTITY);
+        assertEquals(upgradedPublic, PUBLIC_IDENTITY);
+        assertEquals(upgradedPublicProvisional, PUBLIC_PROVISIONAL_IDENTITY);
+    }
 
     @Test
     public void testCreateIdentity() throws IOException {
@@ -93,10 +112,10 @@ public class IdentityTest {
         String provIdentity = Identity.createProvisionalIdentity(appId, email);
         String publicProvIdentity = Identity.getPublicIdentity(provIdentity);
 
-        String newIdentity = Identity.upgradeIdentity(identity);
-        String newPublicIdentity = Identity.upgradeIdentity(publicIdentity);
-        String newProvIdentity = Identity.upgradeIdentity(provIdentity);
-        String newPublicProvIdentity = Identity.upgradeIdentity(publicProvIdentity);
+        String newIdentity = upgradeIdentity(identity);
+        String newPublicIdentity = upgradeIdentity(publicIdentity);
+        String newProvIdentity = upgradeIdentity(provIdentity);
+        String newPublicProvIdentity = upgradeIdentity(publicProvIdentity);
 
         assertEquals(unJson.apply(identity), unJson.apply(newIdentity));
         assertEquals(unJson.apply(publicIdentity), unJson.apply(newPublicIdentity));
