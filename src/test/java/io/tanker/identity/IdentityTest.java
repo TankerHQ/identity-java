f49dc545296c5c9901ad08d8d835d2e0d30fb512
Timothée Isnard <timothee.isnard@tanker.io>
2021-06-22 22:40:12 +0200

    Add tests and vectors for phone_number public provisional identities

--- src/test/java/io/tanker/identity/IdentityTest.java
+++ src/test/java/io/tanker/identity/IdentityTest.java
@@ -21,11 +21,16 @@ public class IdentityTest {
     private static final String appId = "tpoxyNzh0hU9G2i9agMvHyyd+pO6zGCjO9BfhrCLjd4=";
     private static final String appSecret = "cTMoGGUKhwN47ypq4xAXAtVkNWeyUtMltQnYwJhxWYSvqjPVGmXd2wwa7y17QtPTZhn8bxb015CZC/e4ZI7+MQ==";
 
+    private static final String USER_PHONE = "+33611223344";
+    private static final String HASHED_USER_PHONE = "JeaiQAh8x7jcioU2m4ihy+CsHJlyW+4VVSSs5SHFUTw=";
     private static final String PERMANENT_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJ1c2VyIiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSIsImRlbGVnYXRpb25fc2lnbmF0dXJlIjoiVTlXUW9sQ3ZSeWpUOG9SMlBRbWQxV1hOQ2kwcW1MMTJoTnJ0R2FiWVJFV2lyeTUya1d4MUFnWXprTHhINmdwbzNNaUE5cisremhubW9ZZEVKMCtKQ3c9PSIsImVwaGVtZXJhbF9wdWJsaWNfc2lnbmF0dXJlX2tleSI6IlhoM2kweERUcHIzSFh0QjJRNTE3UUt2M2F6TnpYTExYTWRKRFRTSDRiZDQ9IiwiZXBoZW1lcmFsX3ByaXZhdGVfc2lnbmF0dXJlX2tleSI6ImpFRFQ0d1FDYzFERndvZFhOUEhGQ2xuZFRQbkZ1Rm1YaEJ0K2lzS1U0WnBlSGVMVEVOT212Y2RlMEhaRG5YdEFxL2RyTTNOY3N0Y3gwa05OSWZodDNnPT0iLCJ1c2VyX3NlY3JldCI6IjdGU2YvbjBlNzZRVDNzMERrdmV0UlZWSmhYWkdFak94ajVFV0FGZXh2akk9In0=";
     private static final String PROVISIONAL_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJlbWFpbCIsInZhbHVlIjoiYnJlbmRhbi5laWNoQHRhbmtlci5pbyIsInB1YmxpY19lbmNyeXB0aW9uX2tleSI6Ii8yajRkSTNyOFBsdkNOM3VXNEhoQTV3QnRNS09jQUNkMzhLNk4wcSttRlU9IiwicHJpdmF0ZV9lbmNyeXB0aW9uX2tleSI6IjRRQjVUV212Y0JyZ2V5RERMaFVMSU5VNnRicUFPRVE4djlwakRrUGN5YkE9IiwicHVibGljX3NpZ25hdHVyZV9rZXkiOiJXN1FFUUJ1OUZYY1hJcE9ncTYydFB3Qml5RkFicFQxckFydUQwaC9OclRBPSIsInByaXZhdGVfc2lnbmF0dXJlX2tleSI6IlVtbll1dmRUYUxZRzBhK0phRHBZNm9qdzQvMkxsOHpzbXJhbVZDNGZ1cVJidEFSQUc3MFZkeGNpazZDcnJhMC9BR0xJVUJ1bFBXc0N1NFBTSDgydE1BPT0ifQ==";
     private static final String PUBLIC_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJ1c2VyIiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSJ9";
     private static final String OLD_PUBLIC_PROVISIONAL_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJlbWFpbCIsInZhbHVlIjoiYnJlbmRhbi5laWNoQHRhbmtlci5pbyIsInB1YmxpY19lbmNyeXB0aW9uX2tleSI6Ii8yajRkSTNyOFBsdkNOM3VXNEhoQTV3QnRNS09jQUNkMzhLNk4wcSttRlU9IiwicHVibGljX3NpZ25hdHVyZV9rZXkiOiJXN1FFUUJ1OUZYY1hJcE9ncTYydFB3Qml5RkFicFQxckFydUQwaC9OclRBPSJ9";
     private static final String PUBLIC_PROVISIONAL_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJoYXNoZWRfZW1haWwiLCJ2YWx1ZSI6IjB1MmM4dzhFSVpXVDJGelJOL3l5TTVxSWJFR1lUTkRUNVNrV1ZCdTIwUW89IiwicHVibGljX2VuY3J5cHRpb25fa2V5IjoiLzJqNGRJM3I4UGx2Q04zdVc0SGhBNXdCdE1LT2NBQ2QzOEs2TjBxK21GVT0iLCJwdWJsaWNfc2lnbmF0dXJlX2tleSI6Ilc3UUVRQnU5RlhjWElwT2dxNjJ0UHdCaXlGQWJwVDFyQXJ1RDBoL05yVEE9In0=";
+    private static final String PHONE_NUMBER_PROVISIONAL_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJwaG9uZV9udW1iZXIiLCJ2YWx1ZSI6IiszMzYxMTIyMzM0NCIsInB1YmxpY19lbmNyeXB0aW9uX2tleSI6Im42bTlYNUxmMFpuYXo4ZjArc2NoTElCTm0rcGlQaG5zWXZBdlh3MktFQXc9IiwicHJpdmF0ZV9lbmNyeXB0aW9uX2tleSI6InRWVFM5bkh4cjJNZFZ1VFI1Y2x3dzBFWGJ3aXM4SGl4Z1BJTmJRSngxVTQ9IiwicHVibGljX3NpZ25hdHVyZV9rZXkiOiJqcklEaWdTQ25BaTNHbDltSUFTbEFpU2hLQzdkQkxGVVpQOUN4TEdzYkg4PSIsInByaXZhdGVfc2lnbmF0dXJlX2tleSI6IlFIcWNMcjhicjZNM2JQblFtUWczcStxSENycDA1RGJjQnBMUGFUWlkwYTZPc2dPS0JJS2NDTGNhWDJZZ0JLVUNKS0VvTHQwRXNWUmsvMExFc2F4c2Z3PT0ifQ==";
+    private static final String PHONE_NUMBER_PUBLIC_PROVISIONAL_IDENTITY = "eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJwaG9uZV9udW1iZXIiLCJ2YWx1ZSI6IkplYWlRQWg4eDdqY2lvVTJtNGloeStDc0hKbHlXKzRWVlNTczVTSEZVVHc9IiwicHVibGljX2VuY3J5cHRpb25fa2V5IjoibjZtOVg1TGYwWm5hejhmMCtzY2hMSUJObStwaVBobnNZdkF2WHcyS0VBdz0iLCJwdWJsaWNfc2lnbmF0dXJlX2tleSI6ImpySURpZ1NDbkFpM0dsOW1JQVNsQWlTaEtDN2RCTEZVWlA5Q3hMR3NiSDg9In0=";
+
 
     private static final LazySodiumJava LazySodium = new LazySodiumJava(new SodiumJava());
 
@@ -62,6 +67,11 @@ public class IdentityTest {
         assertTrue(LazySodium.cryptoSignVerifyDetached(delegationSignature, signed, signed.length, Base64.getDecoder().decode(appPublicKey)));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testCannotCreateProvisionalIdentityWithBadTarget() {
+       Identity.createProvisionalIdentity(appId, "INVALID !", "xxx");
+    }
+
     @Test
     public void testCreateProvisionalIdentity() {
         String email = "alice@tanker.io";
@@ -72,6 +82,18 @@ public class IdentityTest {
         assertEquals(identityObj.getString("trustchain_id"), appId);
         assertEquals(identityObj.getString("target"), "email");
         assertEquals(identityObj.getString("value"), email);
+    }
+
+    @Test
+    public void testCreateProvisionalPhoneNumberIdentity() {
+        String phone = "+611223344";
+        String identity = Identity.createProvisionalIdentity(appId, "phone_number", phone);
+
+        JsonObject identityObj = Json.createReader(new ByteArrayInputStream(Base64.getDecoder().decode(identity))).readObject();
+
+        assertEquals(identityObj.getString("trustchain_id"), appId);
+        assertEquals(identityObj.getString("target"), "phone_number");
+        assertEquals(identityObj.getString("value"), phone);
     }
 
     @Test
@@ -100,6 +122,33 @@ public class IdentityTest {
         assertEquals(publicIdentityObj.getString("public_encryption_key"), identityObj.getString("public_encryption_key"));
         assertEquals(publicIdentityObj.getString("target"), "hashed_email");
         assertEquals(publicIdentityObj.getString("trustchain_id"), appId);
+    }
+
+    @Test
+    public void testParsePhoneNumberProvisionalIdentity() {
+        JsonObject identityObj = Json.createReader(new ByteArrayInputStream(Base64.getDecoder().decode(PHONE_NUMBER_PROVISIONAL_IDENTITY))).readObject();
+
+        assertEquals(identityObj.getString("target"), "phone_number");
+        assertEquals(identityObj.getString("value"), USER_PHONE);
+        assertEquals(identityObj.getString("trustchain_id"), appId);
+        assertEquals(identityObj.getString("public_signature_key"), "jrIDigSCnAi3Gl9mIASlAiShKC7dBLFUZP9CxLGsbH8=");
+        assertEquals(identityObj.getString("public_encryption_key"), "n6m9X5Lf0Znaz8f0+schLIBNm+piPhnsYvAvXw2KEAw=");
+        assertEquals(identityObj.getString("private_signature_key"), "QHqcLr8br6M3bPnQmQg3q+qHCrp05DbcBpLPaTZY0a6OsgOKBIKcCLcaX2YgBKUCJKEoLt0EsVRk/0LEsaxsfw==");
+        assertEquals(identityObj.getString("private_encryption_key"), "tVTS9nHxr2MdVuTR5clww0EXbwis8HixgPINbQJx1U4=");
+    }
+
+    @Test
+    public void testParsePhoneNumberPublicProvisionalIdentity() {
+        String publicIdentity = Identity.getPublicIdentity(PHONE_NUMBER_PROVISIONAL_IDENTITY);
+        assertEquals(publicIdentity, PHONE_NUMBER_PUBLIC_PROVISIONAL_IDENTITY);
+
+        JsonObject publicIdentityObj = Json.createReader(new ByteArrayInputStream(Base64.getDecoder().decode(publicIdentity))).readObject();
+
+        assertEquals(publicIdentityObj.getString("target"), "phone_number");
+        assertEquals(publicIdentityObj.getString("value"), HASHED_USER_PHONE);
+        assertEquals(publicIdentityObj.getString("trustchain_id"), appId);
+        assertEquals(publicIdentityObj.getString("public_signature_key"), "jrIDigSCnAi3Gl9mIASlAiShKC7dBLFUZP9CxLGsbH8=");
+        assertEquals(publicIdentityObj.getString("public_encryption_key"), "n6m9X5Lf0Znaz8f0+schLIBNm+piPhnsYvAvXw2KEAw=");
     }
 
     @Test
